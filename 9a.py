5bb0a9d5ce85ee0c854fc2b13ec1fa96ed3a7a4a
Kendall Yu <yutienmu@gmail.com>
2023-12-09 00:23:53 -0500

    day 9

---
+++ 9a.py
@@ -0,0 +1,21 @@
+import sys
+
+import collections
+import math
+
+sys.setrecursionlimit(10000)
+
+def string_to_int_list(l, delimiter=None):
+    return list(map(int, l.split(delimiter)))
+
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+
+n = 0
+for line in lines:
+    line = string_to_int_list(line)
+    while set(line) != {0}:
+        n += line[-1]
+        line = [b - a for a, b in zip(line, line[1:])]
+
+print(n)
